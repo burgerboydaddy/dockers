86f399909382cc19cbacdfd93acc7f2e8f268347
Vladimir Ilic <burger.boy.daddy@gmail.com>
2016-03-26 00:02:12 -0700

    first commit for nodemon docker

---
+++ nodemonDocker/index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log('received request');
+  res.end('My port is 8081\n');
+}).listen(8081);
+console.log('Server running on port ' + process.env.PORT);
+console.log('I am joking, port is 8081');
